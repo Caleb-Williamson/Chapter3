b7c6857aa4b008d1da954fe367da39c24730ca68
Caleb_Williamson <calebdogboss@mail.fresnostate.edu>
2022-03-22 15:22:20 -0700

    3-2

---
+++ 3-2.py
@@ -0,0 +1,21 @@
+# ## Determine the larger fo two numbers.
+# # Obtain the two numbers from the user.
+
+# num1 = eval(input("Enter the first number: "))
+# num2 = eval(input("Enter the second number: "))
+
+# # Determine and disokat the larger value.
+# if num1 > num2:
+#     largerValue = num1 #Execute this statement if the condition is true
+# else:
+#     largerValue = num2 #Execute this statement if the condition is false
+# print("The larger value is", str(largerValue) + ".")
+
+
+# answer = eval(input("How many gallons does a ten-gallon hat hold?"))
+
+# if (0.5 <= answer <=1):
+#     print("Good, ", end="")
+# else: 
+#     print("No, ", end="")
+# print("it holds about 3/4 of a gallon.")
